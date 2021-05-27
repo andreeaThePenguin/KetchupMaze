72c09266d33db4e1e3ffde18f4f7dd1a48f2b352
miuAndreea <69588288+miuAndreea@users.noreply.github.com>
2021-05-27 11:02:47 +0300

    Intrare meniuri
    Definirea meniurilor jocului + a punctului de intrare in ele, peste cele ale proiectului sursa.
    (Menu game easy, medium, hard si menu level)

--- src/menus/Menus.c
+++ src/menus/Menus.c
@@ -3,9 +3,13 @@
 #include <Library/BaseLib.h>
 #include "Menus.h"
 
+// Aici se face intrarea in functiile tuturor meniurilor posibile
+
 MENU EnterMainMenu(BOOLEAN first);
 MENU EnterSetupMenu();
 MENU EnterBootMenu();
+MENU EnterGameMenu();
+MENU EnterLevelMenu();
 
 void StartMenus() {
     MENU current_menu = MENU_MAIN_MENU;
@@ -30,6 +34,22 @@ void StartMenus() {
                 gRT->ResetSystem(EfiResetShutdown, 0, 0, "shutdown");
                 CpuDeadLoop();
                 break;
+
+	    case MENU_GAME_EASY:
+		current_menu = EnterGameMenuEasy();
+		break;
+
+	    case MENU_GAME_MEDIUM:
+                current_menu = EnterGameMenuMedium();
+                break;
+
+	    case MENU_GAME_HARD:
+                current_menu = EnterGameMenuHard();
+                break;
+
+	    case MENU_LEVEL:
+		current_menu = EnterLevelMenu();
+		break;
         }
 
         first = FALSE;
