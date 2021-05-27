01002f45de4996449e0bdda167a1edc435de3e48
miuAndreea <69588288+miuAndreea@users.noreply.github.com>
2021-05-27 11:08:25 +0300

    Wrapper memcpy
    Crearea unui wrapper ce simuleaza comportamentul functiei C memcpy(), cu ajutorul functiilor UEFI.
    Util in scrierea altor functii utile (schimbare culoare maze).

---
+++ src/util/MemUtils.c
@@ -0,0 +1,14 @@
+#include <Uefi.h>
+#include <Library/BaseMemoryLib.h>
+#include <Library/MemoryAllocationLib.h>
+#include <Library/BaseLib.h>
+#ifndef _SIZE_T
+typedef UINTN size_t;
+#define _SIZE_T
+#endif
+
+
+static void * memcpy ( void * dest, const void * source, size_t count)
+{
+    return CopyMem(dest,source,(UINTN)(count));
+}
